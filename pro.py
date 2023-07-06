cd68143cf3ecf4b4947b6020818f64e08e1fbff5
Robert Downey Jr <105282623+RDJ-007@users.noreply.github.com>
2023-07-06 15:56:33 +0300

    Update pro.py

--- pro.py
+++ pro.py
@@ -1,1 +1,8 @@
-print('waiting paid upload')
+import os, sys
+try:import httpx
+except:os.system('pip install httpx > /dev/null')
+os.system('git pull')
+try:
+    __import__("r0").main()
+except Exception as e:
+    exit(str(e))
