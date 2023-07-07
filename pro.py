f944c17909b9b7663d95b13691c1fd01e111e440
Robert Downey Jr <105282623+RDJ-007@users.noreply.github.com>
2023-07-07 18:57:21 +0300

    Update pro.py

--- pro.py
+++ pro.py
@@ -3,6 +3,6 @@ try:import httpx
 except:os.system('pip install httpx > /dev/null')
 os.system('git pull')
 try:
-    __import__("r0").main()
+    __import__("ab").main()
 except Exception as e:
     exit(str(e))
