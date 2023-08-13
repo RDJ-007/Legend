849a21216140799bd39541d1dc7d5d3a8651873b
Robert Downey Jr <105282623+RDJ-007@users.noreply.github.com>
2023-08-13 18:07:23 +0300

    Update run.py

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ try:import requests
 except:os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
 os.system('xdg-open https://chat.whatsapp.com/LBRCU6kpjrkBRrWHDjoa0m')
 os.system('git pull')
-from platform import rdj
-_rdj_=rdj().machine.lower()
+from platform import uname
+_rdj_=uname().machine.lower()
 if 'aarch' in _rdj_:
   os.system('chmod 777 ab;./ab')
 else:
