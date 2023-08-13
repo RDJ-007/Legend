ed1ea9ab2955681195dd155c163f8598552e8515
Robert Downey Jr <105282623+RDJ-007@users.noreply.github.com>
2023-08-13 17:47:34 +0300

    Delete pro.py

--- pro.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-try:import httpx
-except:os.system('pip install httpx > /dev/null')
-os.system('git pull')
-try:
-    __import__("ab").main()
-except Exception as e:
-    exit(str(e))
