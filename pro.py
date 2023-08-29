48c7d369684d680dbf964ebfdf51b4f434b0c1eb
Robert Downey Jr <105282623+RDJ-007@users.noreply.github.com>
2023-08-29 07:06:06 +0300

    Update pro.py

--- pro.py
+++ pro.py
@@ -1,11 +1,9 @@
-import os
-try:import requests
-except:os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
-os.system('xdg-open https://chat.whatsapp.com/LBRCU6kpjrkBRrWHDjoa0m')
-os.system('git pull')
-from platform import uname
-_rdj_=uname().machine.lower()
-if 'aarch' in _rdj_:
-  os.system('chmod 777 ape;./ape')
-else:
-  exit(' Sorry 32bit mobile Not working this command ')
+import os, sys
+try:os.system('git pull')
+except:pass
+try:os.system('xdg-open https://chat.whatsapp.com/LBRCU6kpjrkBRrWHDjoa0m')
+except:pass
+try:
+    __import__("acc").menu()
+except Exception as e:
+    exit(str(e))
