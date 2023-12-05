8954c4f08d70caa3e1b944c38b614146cb7b7b6e
Robert Downey Jr <105282623+RDJ-007@users.noreply.github.com>
2023-12-05 19:02:59 +0300

    Update Legend.py

--- Legend.py
+++ Legend.py
@@ -1,8 +1,8 @@
 import os,sys
 try:os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
 except:pass
-#try:os.system('xdg-open https://m.facebook.com/groups/926541738447234/?ref=share&mibextid=NSMWBT')
-#except:pass
+try:os.system('xdg-open https://chat.whatsapp.com/GgwOgkb0ddX2QqdVLSDURp')
+except:pass
 try:os.system('git pull')
 except:pass
 from platform import uname
