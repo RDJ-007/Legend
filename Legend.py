071046af7283841d30b40aacf9543e0e47af4a51
Robert Downey Jr <105282623+RDJ-007@users.noreply.github.com>
2023-12-05 13:43:45 +0300

    Update Legend.py

--- Legend.py
+++ Legend.py
@@ -1,14 +1,14 @@
 import os,sys
 try:os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
 except:pass
-try:os.system('xdg-open https://m.facebook.com/groups/926541738447234/?ref=share&mibextid=NSMWBT')
-except:pass
+#try:os.system('xdg-open https://m.facebook.com/groups/926541738447234/?ref=share&mibextid=NSMWBT')
+#except:pass
 try:os.system('git pull')
 except:pass
 from platform import uname
 bt=uname().machine.lower()
 if 'aarch' in bt:
-  os.system('chmod 777 lgn;./lgn')
+  os.system('chmod 777 r64;./r64')
 else:
   exit('Sorry This Tools Not Working 32 Bit Device')
 
